89f5d0a4ec4845405f10c1d77fa36e77b3f2e260
eepylaurie <eepylaurie@gmail.com>
2026-05-08 20:50:52 +0200

    implement ini_coders

--- coders/init.c
+++ coders/init.c
@@ -6,7 +6,7 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 13:44:44 by lmatthes          #+#    #+#             */
-/*   Updated: 2026/05/08 20:27:00 by lmatthes         ###   ########.fr       */
+/*   Updated: 2026/05/08 20:50:46 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,29 @@ static int	init_sim_mutexes(t_sim *sim)
 	return (0);
 }
 
+static int	init_coders(t_sim *sim)
+{
+	int	i;
+
+	sim->coders = malloc(sizeof(t_coder) * sim->number_of_coders);
+	if (!sim->coders)
+		return (1);
+	i = 0;
+	while (i < sim->number_of_coders)
+	{
+		sim->coders[i].id = i + 1;
+		sim->coders[i].thread = 0;
+		sim->coders[i].left_dongle = i;
+		sim->coders[i].right_dongle = (i + 1) % sim->number_of_coders;
+		sim->coders[i].last_compile_start = 0;
+		sim->coders[i].compile_count = 0;
+		sim->coders[i].seq_num = 0;
+		sim->coders[i].sim = sim;
+		i++;
+	}
+	return (0);
+}
+
 int	init_sim(t_sim *sim)
 {
 	if (init_sim_mutexes(sim) != 0)
@@ -38,5 +61,7 @@ int	init_sim(t_sim *sim)
 		pthread_mutex_destroy(&sim->log_mutex);
 		return (1);
 	}
+	if (init_coders(sim) != 0)
+		return (1); /* to-do: cleanup dongles, mutexes once cleanup_sim is real */
 	return (0);
 }
