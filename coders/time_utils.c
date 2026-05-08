86c31e1edf6564a3a6f59314dcd807374a1f6c95
lmatthes <lmatthes@student.42heilbronn.de>
2026-05-08 13:47:55 +0200

    implement time utilities

--- coders/time_utils.c
+++ coders/time_utils.c
@@ -6,27 +6,30 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 13:45:00 by lmatthes          #+#    #+#             */
-/*   Updated: 2026/05/08 13:06:41 by lmatthes         ###   ########.fr       */
+/*   Updated: 2026/05/08 13:47:30 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// get_timestamp_ms() and precise_sleep().
 
 #include "codexion.h"
 
 long	get_current_ms(void)
 {
-	return (0);
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000L + tv.tv_usec / 1000);
 }
 
 long	get_timestamp_ms(long start_ms)
 {
-	(void)start_ms;
-	return (0);
+	return (get_current_ms() - start_ms);
 }
 
 void	precise_sleep(long duration_ms, t_sim *sim)
 {
-	(void)duration_ms;
-	(void)sim;
+	long	target;
+
+	target = get_current_ms() + duration_ms;
+	while (get_current_ms() < target && !simulation_stopped(sim))
+		usleep(200);
 }
