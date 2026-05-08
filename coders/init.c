439d3fbe87b2b9309dfd4574b716d6e1cb0c6f0b
lmatthes <lmatthes@student.42heilbronn.de>
2026-05-08 16:04:57 +0200

    implement init_sim_mutexes

--- coders/init.c
+++ coders/init.c
@@ -6,7 +6,7 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 13:44:44 by lmatthes          #+#    #+#             */
-/*   Updated: 2026/05/08 13:04:49 by lmatthes         ###   ########.fr       */
+/*   Updated: 2026/05/08 16:04:40 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,25 @@
 
 #include "codexion.h"
 
+static int	init_sim_mutexes(t_sim *sim)
+{
+	if (pthread_mutex_init(&sim->stop_mutex, NULL) != 0)
+	{
+		fprintf(stderr, "Error: failed to init stop_mutex\n");
+		return (1);
+	}
+	if (pthread_mutex_init(&sim->log_mutex, NULL) != 0)
+	{
+		fprintf(stderr, "Error: failed to init log_mutex\n");
+		pthread_mutex_destroy(&sim->stop_mutex);
+		return (1);
+	}
+	return (0);
+}
+
 int	init_sim(t_sim *sim)
 {
-	(void)sim;
+	if (init_sim_mutexes(sim) != 0)
+		return (1);
 	return (0);
 }
