7ef0c19fbbfd791cfd505b45b6114d3d24355998
lmatthes <lmatthes@student.42heilbronn.de>
2026-05-11 17:09:06 +0200

    implement thread-safe logging

--- coders/log.c
+++ coders/log.c
@@ -6,23 +6,29 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 13:44:48 by lmatthes          #+#    #+#             */
-/*   Updated: 2026/05/08 13:10:22 by lmatthes         ###   ########.fr       */
+/*   Updated: 2026/05/11 17:04:13 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// log_msg() with the log mutex.
 
 #include "codexion.h"
 
 void	log_msg(t_sim *sim, int coder_id, char *msg)
 {
-	(void)sim;
-	(void)coder_id;
-	(void)msg;
+	long	timestamp;
+
+	timestamp = get_timestamp_ms(sim->start_ms);
+	pthread_mutex_lock(&sim->log_mutex);
+	if (!simulation_stopped(sim))
+		printf("%ld %d %s\n", timestamp, coder_id, msg);
+	pthread_mutex_unlock(&sim->log_mutex);
 }
 
 void	log_burnout(t_sim *sim, int coder_id)
 {
-	(void)sim;
-	(void)coder_id;
+	long	timestamp;
+
+	timestamp = get_timestamp_ms(sim->start_ms);
+	pthread_mutex_lock(&sim->log_mutex);
+	printf("%ld %d burned out\n", timestamp, coder_id);
+	pthread_mutex_unlock(&sim->log_mutex);
 }
