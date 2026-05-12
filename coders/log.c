fed049550c019ae6badc29516e28d0b04ebb3d2d
eepylaurie <eepylaurie@gmail.com>
2026-05-12 14:58:56 +0200

    fix by moving timestamp capture inside log_mutex

--- coders/log.c
+++ coders/log.c
@@ -6,7 +6,7 @@
 /*   By: lmatthes <lmatthes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/06 13:44:48 by lmatthes          #+#    #+#             */
-/*   Updated: 2026/05/11 17:04:13 by lmatthes         ###   ########.fr       */
+/*   Updated: 2026/05/12 14:56:24 by lmatthes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	log_msg(t_sim *sim, int coder_id, char *msg)
 {
 	long	timestamp;
 
-	timestamp = get_timestamp_ms(sim->start_ms);
 	pthread_mutex_lock(&sim->log_mutex);
+	timestamp = get_timestamp_ms(sim->start_ms);
 	if (!simulation_stopped(sim))
 		printf("%ld %d %s\n", timestamp, coder_id, msg);
 	pthread_mutex_unlock(&sim->log_mutex);
@@ -27,8 +27,8 @@ void	log_burnout(t_sim *sim, int coder_id)
 {
 	long	timestamp;
 
-	timestamp = get_timestamp_ms(sim->start_ms);
 	pthread_mutex_lock(&sim->log_mutex);
+	timestamp = get_timestamp_ms(sim->start_ms);
 	printf("%ld %d burned out\n", timestamp, coder_id);
 	pthread_mutex_unlock(&sim->log_mutex);
 }
